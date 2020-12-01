8833dcfc37f1e18a4a52c5bf6354f950d46a0809
Ezekiel Elin <ezekielelin@me.com>
2020-12-01 18:28:49 -0500

    Fixed typo in Day 1

--- Sources/AdventOfCode/2020/2020 Day 1.swift
+++ Sources/AdventOfCode/2020/2020 Day 1.swift
@@ -12,11 +12,11 @@ struct Day2020_1: Day {
     func run(input: String) {
         let lines = input.split(whereSeparator: \.isNewline).map { Int($0)! }
     
-        routine(parameter: 2)
-        routine(parameter: 3)
+        routine(lines: lines, parameter: 2)
+        routine(lines: lines, parameter: 3)
     }
     
-    func routine(parameter: Int) {
+    func routine(lines: [Int], parameter: Int) {
         for combo in lines.combinations(ofCount: parameter) {
             if combo.reduce(0, +) == 2020 {
                 print(combo.reduce(1, *))
