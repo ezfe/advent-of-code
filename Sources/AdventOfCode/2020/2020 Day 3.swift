e1e10ae6888bde827d83efcc66d8524efbdf4fc4
Ezekiel Elin <ezekielelin@me.com>
2020-12-03 21:28:18 -0500

    Day 3

---
+++ Sources/AdventOfCode/2020/2020 Day 3.swift
@@ -0,0 +1,49 @@
+//
+//  2020 Day 3.swift
+//  
+//
+//  Created by Ezekiel Elin on 10/20/20.
+//
+
+import Foundation
+
+struct Day2020_3: Day {
+    enum Cell {
+        case open, tree
+    }
+    
+    func run(input: String) {
+        let lines: [[Cell]] = input.split(whereSeparator: \.isNewline).map(Array.init).map { chars in
+            return chars.map { char in
+                if char == "." {
+                    return Cell.open
+                } else if char == "#" {
+                    return Cell.tree
+                } else {
+                    print("Error!")
+                    return Cell.open
+                }
+            }
+        }
+        
+        var sum = 1
+        
+        for (xSlope, ySlope) in [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)] {
+            var y = 0
+            var x = 0
+            var trees = 0
+            repeat {
+                if lines[y][x % lines[y].count] == .tree {
+                    trees += 1
+                }
+                x += xSlope
+                y += ySlope
+            } while y < lines.count
+            
+            print((xSlope, ySlope), trees)
+            sum *= trees
+        }
+        print("Part 2: \(sum)")
+    }
+    
+}
--- Sources/AdventOfCode/2020/2020 Index.swift
+++ Sources/AdventOfCode/2020/2020 Index.swift
@@ -10,4 +10,5 @@ import Foundation
 let entryPoints2020: [Int: Day] = [
     1: Day2020_1(),
     2: Day2020_2(),
+    3: Day2020_3()
 ]
