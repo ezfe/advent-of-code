bde56cf19b681a118dc0db0e91433b99c3baea0f
Ezekiel Elin <ezekielelin@me.com>
2020-12-04 14:16:46 -0500

    Day 4

--- Sources/AdventOfCode/2020/2020 Index.swift
+++ Sources/AdventOfCode/2020/2020 Index.swift
@@ -10,5 +10,6 @@ import Foundation
 let entryPoints2020: [Int: Day] = [
     1: Day2020_1(),
     2: Day2020_2(),
-    3: Day2020_3()
+    3: Day2020_3(),
+    4: Day2020_4()
 ]
--- Sources/AdventOfCode/main.swift
+++ Sources/AdventOfCode/main.swift
@@ -6,7 +6,7 @@ struct AOCLauncher: ParsableCommand {
     var year: Int = 2020
     
     @Argument(help: "The day to run")
-    var day: Int = 3
+    var day: Int = 4
 
     @Argument(help: "The input file path")
     var filePath: String?
