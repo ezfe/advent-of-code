3d8fa0e84a80bd2717d6c9ba58d35c23bf5f5545
Ezekiel Elin <ezekielelin@me.com>
2020-12-05 00:28:16 -0500

    Improvements to Day 5

--- Sources/AdventOfCode/2020/2020 Day 5.swift
+++ Sources/AdventOfCode/2020/2020 Day 5.swift
@@ -9,11 +9,8 @@ import Foundation
 
 struct Day2020_5: Day {
     func run(input: String) {
-        let lines = input.split(whereSeparator: \.isNewline)
-        
-        let seatIDs = lines
-            .map { identify(String($0)) }
-            .map { $0.0 * 8 + $0.1 }
+        let lines = input.split(whereSeparator: \.isNewline).map(String.init)
+        let seatIDs = lines.map(alternative(instructions:))
         
         let max = seatIDs.max()!
         let min = seatIDs.min()!
@@ -28,36 +25,49 @@ struct Day2020_5: Day {
         print(mySeats.first!)
     }
     
-    enum Ins: String {
-        case lower = "F", upper = "B", left = "L", right = "R"
-    }
-    
-    func identify(_ _instructions: String) -> (Int, Int) {
-        let instructions = _instructions.map { char in
-            return Ins.init(rawValue: "\(char)")!
-        }
-        
-        let rowIns = instructions[0..<7]
-        let row = identify(instrs: Array(rowIns))
-        let colIns = instructions[7...]
-        let col = identify(instrs: Array(colIns))
-        
-        return (row, col)
-    }
-    
-    func identify(instrs: [Ins]) -> Int {
-        var size = Int(pow(Double(2), Double(instrs.count))) - 1
-        var lower = 0
-        for ins in instrs {
-            switch ins {
-                case .upper, .right:
-                    lower += size / 2 + 1
-                    size /= 2
-                case .lower, .left:
-                    size /= 2
+    func alternative(instructions: String) -> Int {
+        let binaryString = String(instructions.map {
+            switch $0 {
+                case "F", "L":
+                    return "0"
+                default: // B, R
+                    return "1"
             }
-        }
-        
-        return lower
+        })
+        return Int(binaryString, radix: 2)!
     }
+    
+    
+//    enum Ins: String {
+//        case lower = "F", upper = "B", left = "L", right = "R"
+//    }
+//
+//    func identify(_ _instructions: String) -> (Int, Int) {
+//        let instructions = _instructions.map { char in
+//            return Ins.init(rawValue: "\(char)")!
+//        }
+//
+//        let rowIns = instructions[0..<7]
+//        let row = identify(instrs: Array(rowIns))
+//        let colIns = instructions[7...]
+//        let col = identify(instrs: Array(colIns))
+//
+//        return (row, col)
+//    }
+//
+//    func identify(instrs: [Ins]) -> Int {
+//        var size = Int(pow(Double(2), Double(instrs.count))) - 1
+//        var lower = 0
+//        for ins in instrs {
+//            switch ins {
+//                case .upper, .right:
+//                    lower += size / 2 + 1
+//                    size /= 2
+//                case .lower, .left:
+//                    size /= 2
+//            }
+//        }
+//
+//        return lower
+//    }
 }
