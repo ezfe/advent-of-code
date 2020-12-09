b83b1d2f15ac404abd02505b2e6ea5922eb75353
Ezekiel Elin <ezekielelin@me.com>
2020-12-09 12:11:36 -0500

    Improved speed of day 9 drastically

--- Sources/AdventOfCode/2020/2020 Day 9.swift
+++ Sources/AdventOfCode/2020/2020 Day 9.swift
@@ -12,30 +12,41 @@ struct Day2020_9: Day {
         let lines = input.split(whereSeparator: \.isNewline).map { Int($0)! }
         
         let p1 = part1(lines: lines)
-        let p2 = part2(lines: lines, part1: p1)
-        
         print(p1)
+
+        let p2 = part2(lines: lines, part1: p1)
         print(p2)
     }
     
     func part1(lines: [Int]) -> Int {
-        part1MainLoop: for i in 25..<lines.count {
-            let range = lines[(i - 25)..<i]
-            for combination in range.combinations(ofCount: 2) {
-                if combination.reduce(0, +) == lines[i] {
-                    continue part1MainLoop
+        func check(list: ArraySlice<Int>, targetSum: Int) -> Bool {
+            for a in list {
+                let b = targetSum - a
+                if list.contains(b) {
+                    return true
                 }
             }
-            return lines[i]
+            return false
+        }
+        
+        for i in 25..<lines.count {
+            let range = lines[(i - 25)..<i]
+            if !check(list: range, targetSum: lines[i]) {
+                return lines[i]
+            }
         }
         exit(1)
     }
     
     func part2(lines: [Int], part1 p1: Int) -> Int {
-        part2MainLoop: for lower in 0..<lines.count {
-            for upper in lower..<lines.count {
+        ml: for lower in 0..<lines.count {
+            for upper in (lower + 1)..<lines.count {
                 let subset = lines[lower...upper]
-                if subset.reduce(0, +) == p1 {
+                let sum = subset.reduce(0, +)
+
+                if sum > p1 {
+                    continue ml
+                } else if sum == p1 {
                     return subset.min()! + subset.max()!
                 }
             }
