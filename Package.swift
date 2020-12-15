24e213d3c9ab350a93c91807598f8445dc315349
Ezekiel Elin <ezekielelin@me.com>
2020-12-14 22:31:53 -0500

    Days 12-14

--- Package.swift
+++ Package.swift
@@ -9,6 +9,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "0.3.1"),
         .package(url: "https://github.com/apple/swift-algorithms", from: "0.0.1"),
+        .package(url: "https://github.com/apple/swift-atomics.git", from: "0.0.2")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -18,6 +19,7 @@ let package = Package(
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
                 .product(name: "Algorithms", package: "swift-algorithms"),
+                .product(name: "Atomics", package: "swift-atomics"),
             ]),
     ]
 )
---
+++ Sources/AdventOfCode/2020/2020 Day 13.swift
@@ -0,0 +1,97 @@
+//
+//  2020 Day 13.swift
+//  
+//
+//  Created by Ezekiel Elin on 12/13/20.
+//
+
+import Foundation
+import Atomics
+
+struct Day2020_13: Day {
+    func run(input: String) {
+        let lines = input.split(whereSeparator: \.isNewline)
+        
+        part1(lines: lines)
+        part2(lines: lines)
+    }
+    
+    func part1<T: StringProtocol>(lines: [T]) {
+        let earliestDeparture = Int(lines[0])!
+        let routes = lines[1].split(separator: ",").compactMap { rid in
+            return Int(rid)
+        }
+        
+        var soonestTime = Int.max
+        var soonestId = Int.max
+
+        for route in routes {
+            let sinceMostRecent = earliestDeparture % route
+            if sinceMostRecent == 0 {
+                soonestTime = 0
+                soonestId = route
+                break
+            } else {
+                let next = (route - sinceMostRecent)
+                if next < soonestTime {
+                    soonestTime = next
+                    soonestId = route
+                }
+            }
+        }
+        
+        print(soonestId * soonestTime)
+    }
+    
+    func part2<T: StringProtocol>(lines: [T]) {
+        let routes = lines[1].split(separator: ",").map { rid in
+            return Int(rid)
+        }.enumerated().compactMap { (eseqel) -> (index: Int, route: Int)? in
+            if let el = eseqel.element {
+                return (index: eseqel.offset, route: el)
+            } else {
+                return nil
+            }
+        }.sorted { (i1, i2) -> Bool in
+            i1.route > i2.route
+        }
+        
+        let k = 100_000_000_000_000 / routes[0].route
+        let queue = DispatchQueue(label: "processing-queue", qos: .userInitiated, attributes: .concurrent)
+        
+        let found = ManagedAtomic(false)
+        
+        let group = DispatchGroup()
+        
+        for i in 0..<8 {
+            group.enter()
+            queue.async {
+                part2Process(k: k + i, routes: routes)
+                group.leave()
+            }
+        }
+
+        group.wait()
+        
+        func part2Process(k: Int, routes: [(index: Int, route: Int)]) {
+            var k = k
+            while !found.load(ordering: .relaxed) {
+                let target = routes[0].route * k
+                var failed = false
+                for (i, route) in routes {
+                    if !(target + i).isMultiple(of: route) {
+                        failed = true
+                        break
+                    }
+                }
+                if failed {
+                    k += 4
+                } else {
+                    found.store(true, ordering: .relaxed)
+                    print(routes[0].route * k)
+                    break
+                }
+            }
+        }
+    }
+}
