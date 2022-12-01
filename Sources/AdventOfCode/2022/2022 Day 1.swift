932d7a96e389b7cc8779128cab98b481d94841e8
Ezekiel Elin <ezekielelin@me.com>
2022-12-01 00:16:47 -0500

    Remove imports

--- Sources/AdventOfCode/2022/2022 Day 1.swift
+++ Sources/AdventOfCode/2022/2022 Day 1.swift
@@ -5,9 +5,6 @@
 //  Created by Ezekiel Elin on 12/1/20.
 //
 
-import Foundation
-import Algorithms
-
 struct Day2022_1: Day {
 	func run(input: String) {
 		let elves = input.split(separator: #/\n\n/#).map { $0.split(whereSeparator: \.isNewline).map { Int($0)! } }
